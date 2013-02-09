3a179092c88b96f573559421dc166927d49a375f
SergeyBerdnikovich <atom@tut.by>
2013-02-09 16:14:16 +0300

    added controller pages#welcome, added root path, deleted public/index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TrestWood::Application.routes.draw do
+  get "pages/welcome"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ TrestWood::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#welcome'
 
   # See how all your routes lay out with "rake routes"
 
