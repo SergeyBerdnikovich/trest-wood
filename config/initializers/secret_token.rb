0df84634422f6733220edeecbb4d2f7ac8eb3088
SergeyBerdnikovich <atom@tut.by>
2013-02-09 16:04:55 +0300

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrestWood::Application.config.secret_token = 'f27e18d3606da511a2ff711d3206b831618df4db15435ea8ca67926de989c90c9a577c93eb494ce8b2244ce2f6d6a23f57efef3333ee0bd8828f1c9ba506816e'
