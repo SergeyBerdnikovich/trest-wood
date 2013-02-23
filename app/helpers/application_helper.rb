b6a22cd5a335a2ab4d0043b25b9842a4449b5412
SergeyBerdnikovich <atom@tut.by>
2013-02-23 14:17:21 +0300

    added sitemap.xml, google analitic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def google_analytics
+    html = ""
+      html << "<script type='text/javascript'>"
+      html <<   "var _gaq = _gaq || [];"
+      html <<   "_gaq.push(['_setAccount', 'UA-38745844-1']);"
+      html <<   "_gaq.push(['_trackPageview']);"
+      html <<   "(function() {"
+      html <<     "var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;"
+      html <<     "ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';"
+      html <<     "var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);"
+      html <<   "})();"
+      html << "</script>"
+  end
 end
